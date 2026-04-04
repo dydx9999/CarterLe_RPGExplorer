6d468428063e717a44e637bfaa3a17508c314bcb
Carter Le <carteryt1324@gmail.com>
2026-04-04 00:11:36 -0400

    Update login.php login form

--- login.php
+++ login.php
@@ -8,7 +8,24 @@
 </head>
 
 <body>
-
+    <header class="site-header">
+        <a class="brand" href="index.php">Quest Explorer</a>
+        <nav>
+            <ul>
+                <li><a href="login.php">Login</a></li>
+                <li><a href="register.php">Register</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main class="site-main">
+        <h1>User Login</h1>
+        <form action="login.php" method="post">
+            <label for="username">Username:</label>
+            <input type="text" name="username" id="username"><br>
+            <label for=" password">Password:</label>
+            <input type="password" name="password" id="password"><br>
+            <input type="submit" value="Log In">
+        </form>
 </body>
 
 </html>
