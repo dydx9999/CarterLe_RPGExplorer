871fdc9dd9165328668de695e6d46aaf1d51b4f8
Carter Le <carteryt1324@gmail.com>
2026-04-03 23:38:25 -0400

    Add navigation bar for user register & login

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RPGExplorer - Landing</title>
+</head>
+
+<body>
+    <header>
+        <a href="index.php">Quest Explorer</a>
+        <nav>
+            <ul>
+                <li><a href="login.php">Login</a></li>
+                <li><a href="register.php">Register</a></li>
+
+            </ul>
+        </nav>
+
+    </header>
+</body>
+
+</html>
