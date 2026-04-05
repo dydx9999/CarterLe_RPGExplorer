ad98e970d42c7ec3d6d869b8163c528ff580023f
Carter Le <carteryt1324@gmail.com>
2026-04-04 23:49:54 -0400

    Add basic authentication for protected content pages

---
+++ conclusion.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+if (!isset($_SESSION['user_id'])) {
+    header('Location: login.php');
+}
+;
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RPG Explorer - Conclusion</title>
+</head>
+
+<body>
+    <header class="site-header">
+        <a class="brand" href="index.php">RPG Explorer</a>
+    </header>
+
+
+
+
+
+</body>
+
+</html>
