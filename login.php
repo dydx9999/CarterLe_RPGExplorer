85882606012ca0eacefbd7d0758752ac6f1cfa24
Carter Le <carteryt1324@gmail.com>
2026-04-10 20:26:29 -0400

    Add site favicon

--- login.php
+++ login.php
@@ -50,6 +50,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>RPG Explorer - Login</title>
     <link rel="stylesheet" href="styles.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon_io/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon_io/favicon-32x32.png">
+    <link rel="manifest" href="/site.webmanifest">
 
 </head>
 
